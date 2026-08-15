9b5be39ccb5023d0f717329ddad7695f0d9baa80
Kaio Ryan <kaior0162@gmail.com>
2026-08-14 21:57:30 -0300

    Update Questao_9.py

--- Questao_9.py
+++ Questao_9.py
@@ -1,7 +1,9 @@
-
 n1 = int(input("Digite um numero: "))
 n2 = int(input("Digite um numero: "))
 
-for i in range(n1, n2):
+if n1>n2:
+    n1,n2=n2,n1
 
-    print(i)
+while n1<n2-1:
+    n1+=1
+    print(n1)
