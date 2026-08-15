3a0288b23196e588c59a7bc47acbd09c47b0ef09
Kaio Ryan <kaior0162@gmail.com>
2026-08-14 21:45:08 -0300

    Update Questao_8.py

--- Questao_8.py
+++ Questao_8.py
@@ -1,13 +1,17 @@
-n1 = int(input("Digite um numero: "))
-n2 = int(input("Digite um numero: "))
-n3 = int(input("Digite um numero: "))
-n4 = int(input("Digite um numero: "))
-n5 = int(input("Digite um numero: "))
+soma = 0
+for i in range(5):
+    soma += int(input(f"Digite o {i+1}º numero: "))
 
-soma = n1 + n2 + n3 + n4 + n5
+#n1 = int(input("Digite um numero: "))
+#n2 = int(input("Digite um numero: "))
+#n3 = int(input("Digite um numero: "))
+#n4 = int(input("Digite um numero: "))
+#n5 = int(input("Digite um numero: "))     outro metodo mais simples
+
+#soma = n1 + n2 + n3 + n4 + n5
 
 print(f"A soma dos numeros inseridos é: {soma}")
 
-media = soma / 5
+media = soma/5
 
 print(f"A media dos numeros inseridos é: {media}")
